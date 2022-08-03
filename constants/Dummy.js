6a39b91aedf869d9d7a26467d62e61f4f5fd03fe
Mohammad Ibnu <siibnu99@gmail.com>
2022-08-03 09:46:13 +0700

    update Cooming Soon

---
+++ constants/Dummy.js
@@ -0,0 +1,39 @@
+import { Icons } from "./Images";
+
+export const SosialMedia = [
+  {
+    name: "Line",
+    icon: Icons.Line,
+    link: "https://rajabrawijaya.ub.ac.id/ln",
+  },
+  {
+    name: "Instagram",
+    icon: Icons.Instagram,
+    link: "https://rajabrawijaya.ub.ac.id/ig",
+  },
+  {
+    name: "Twitter",
+    icon: Icons.Twitter,
+    link: "https://rajabrawijaya.ub.ac.id/tw",
+  },
+  {
+    name: "Telegram",
+    icon: Icons.Telegram,
+    link: "https://rajabrawijaya.ub.ac.id/tl",
+  },
+  {
+    name: "Tiktok",
+    icon: Icons.Tiktok,
+    link: "https://rajabrawijaya.ub.ac.id/tt",
+  },
+  {
+    name: "Youtube",
+    icon: Icons.Youtube,
+    link: "https://rajabrawijaya.ub.ac.id/yt",
+  },
+  {
+    name: "Spotify",
+    icon: Icons.Spotify,
+    link: "https://rajabrawijaya.ub.ac.id/sp",
+  },
+];
--- constants/Images.js
+++ constants/Images.js
@@ -4,3 +4,13 @@ export const Background = {
 export const Pattern = {
   HeadingFull: require("../public/assets/images/HeadingFull.svg"),
 };
+
+export const Icons = {
+  Instagram: require("../public/assets/icons/Instagram.png"),
+  Twitter: require("../public/assets/icons/Twitter.png"),
+  Youtube: require("../public/assets/icons/Youtube.png"),
+  Line: require("../public/assets/icons/Line.png"),
+  Telegram: require("../public/assets/icons/Telegram.png"),
+  Tiktok: require("../public/assets/icons/Tiktok.png"),
+  Spotify: require("../public/assets/icons/Spotify.png"),
+};
