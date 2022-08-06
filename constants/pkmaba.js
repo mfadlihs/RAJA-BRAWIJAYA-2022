81d47c6e24f86bccf6c05a9ae3a0209112138531
Hasan Nasrullah <hnasrul22@gmail.com>
2022-08-06 16:34:46 +0700

    insrt img pkmaba

--- constants/pkmaba.js
+++ constants/pkmaba.js
@@ -2,16 +2,17 @@ const dataPk = [
 	{
 		id: "PK2MABA FILKOM_ID",
 		name: "PK2MABA FILKOM",
+		img : '/assets/fakultas/FILKOM.png',
 		fakultas: "Fakultas Ilmu Komputer",
 		description:
-			"PKKMB FILKOM merupakan kegiastan pengenalan kehidupan kampus sendiri terdiri dari dua rangkaian besar, yaitu :<br/><br/> PK2MABA FILKOM merupakan singkatan Pengenalan Kehidupan Kampus bagi Mahasiswa Baru Fakultas Ilmu Komputer sebuah kegiatan yang ditujukan kepada mahasiswa baru dengan memberikan pembekalan mengenai kehidupan kampus, baik dari segi akademik maupun non akademik supaya nantinya mahasiswa baru dapat beradaptasi dengan baik dengan lingkungan kampus.<br/><br/>KRIDA MAHASISWA STARTUP ACADEMY - Rangkaian kedua setelah PK2MABA FILKOM yang memfasilitasi mahasiswa dalam proses adaptasi sosial dan proses pembelajaran di kampus. Krida Mahasiswa Startup academy memiliki 3 (tiga) rangkaian utama, yaitu Development class, open house, dan social campaign",
-		spotify: "spotify/pk2mabafilkom",
-		youtube: "youtube/pk2mabafilkom",
-		tiktok: "tiktok/pk2mabafilkom",
-		telegram: "telegram/pk2mabafilkom",
-		twitter: "twitter/pk2mabafilkom",
-		instagram: "instagram/pk2mabafilkom",
-		line: "line/pk2mabafilkom",
+			"PKKMB FILKOM merupakan kegiatan pengenalan kehidupan kampus sendiri terdiri dari dua rangkaian besar, yaitu :<br/><br/> PK2MABA FILKOM merupakan singkatan Pengenalan Kehidupan Kampus bagi Mahasiswa Baru Fakultas Ilmu Komputer sebuah kegiatan yang ditujukan kepada mahasiswa baru dengan memberikan pembekalan mengenai kehidupan kampus, baik dari segi akademik maupun non akademik supaya nantinya mahasiswa baru dapat beradaptasi dengan baik dengan lingkungan kampus.<br/><br/>KRIDA MAHASISWA STARTUP ACADEMY - Rangkaian kedua setelah PK2MABA FILKOM yang memfasilitasi mahasiswa dalam proses adaptasi sosial dan proses pembelajaran di kampus. Krida Mahasiswa Startup academy memiliki 3 (tiga) rangkaian utama, yaitu Development class, open house, dan social campaign",
+		spotify: null,
+		youtube: null,
+		tiktok: null,
+		telegram: null,
+		twitter: null,
+		instagram: null,
+		line: null,
 	},
 	{
 		id: "PK2MABA FK_ID",
@@ -50,18 +51,18 @@ const dataPk = [
 		description:
 			"PKKMB (Pengenalan Kehidupan Kampus bagi Mahasiswa Baru) FKH merupakan sebuah sarana bagi mahasiswa baru dalam meningkatkan peran civitas akademika Fakultas Kedokteran Hewan Universitas Brawijaya dalam melaksanakan fungsi sebagai Tri Dharma Perguruan Tinggi yaitu Pendidikan, Penelitian, dan Pengabdian Masyarakat. Acara PKKMB ini terdiri dari dua rangkaian besar, yaitu PKKMB (Pengenalan Kehidupan Kampus bagi Mahasiswa Baru) dan KRIMA (Krida Mahasiswa). Adapun tujuan diadakannya kegiatan PKKMB membentukmahasiswa Kedokteran Hewan yang berkarakter, tangguh, beretika dan jujur berdasarkan nilai-nilai moral kemanusiaan dan prinsip 7 Pilar Kolegium Mahasiswa Kedokteran Hewan Universitas Brawijaya yaitu Religius, Moralis, Intelek, Profesional, Kepemimpinan, Prestatif, dan Kolegalitas",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://www.instagram.com/maba_fkhubofficial/",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FIB",
@@ -70,18 +71,18 @@ const dataPk = [
 		description:
 			"PKKMABA FIB merupakan sebuah sarana bagi para mahasiswa baru dalam mengenal kehidupan kampus, khususnya Fakultas Ilmu Budaya secara lebih jauh. Tujuan diadakannya PKKMABA FIB adalah untuk memberi pengenalan yang layak kepada para mahasiswa tentang kehidupan kampus supaya  mahasiswa baru bisa beradaptasi dengan baik terhadap fakultasnya, serta memiliki rasa bangga dan semangat untuk membangun dan menjadikan Fakultas Ilmu Budaya menjadi lebih baik.",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://www.instagram.com/pkkmabafibub/",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA VOKASI",
@@ -90,18 +91,18 @@ const dataPk = [
 		description:
 			"PKKMB atau yang biasa dikenal dengan Pengenalan Kehidupan Kampus Mahasiswa Baru merupakan sebuah kegiatan penyambutan bagi mahasiswa baru khususnya Fakultas Vokasi Universitas Brawijaya. Kegiatan ini merupakan wadah bagi mahasiswa baru untuk membina dan membentuk karakter mahasiswa baru dalam beradaptasi dengan lingkup kampus baik dalam kegiatan belajar mengajar, lingkungan kampus, serta unik kegiatan mahasiswa. Kegiatan PKKMB Vokasi dibagi menjadi 3 (tiga) rangkaian kegiatan, yaitu YUWARAJA, KRIMA (Krida Mahasiswa), serta Kejuaran dan OH",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://www.instagram.com/pkkmb_vokasiub/",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FKG",
@@ -111,18 +112,18 @@ const dataPk = [
 		description:
 			"PROBINMABA merupakan sebuah rangkaian kegiatan kaderisasi dari Kementrian Pengembangan Sumber Daya Manusia yang ditujukan untuk pengenalan dan orientasi yang diadakan oleh FKG UB. Kegiatan ini memiliki tujuan memperkenalkan kepada mahasiswa baru FKG UB tentang kehidupan kampus dan mempersiapkan mahasiswa baru FKG UB 2022 untuk menjadi penerus KM FKG UB berdasarkan 6 nilai KM. Rangkaian PROBINMABA FKG UB 2022 terdiri dari dua rangkaian utama, yaitu PK2Maba yang bertujuan untuk memperkenalkan seluruh jajaran civitas akademik dan PK2M yang bertujuan untuk mengembangkan karakter mahasiswa baru supaya dapat menerapkan serta mengimplementasikan 6 nilai KM FKG.",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://www.instagram.com/probinmabafkgub/",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FMIPA",
@@ -131,18 +132,18 @@ const dataPk = [
 		description:
 			"PROBINMABA merupakan wadah bagi mahasiswa baru FMIPA UB 2022 untuk mendapatkan pembekalan atau arahan dan penanaman nilai supaya dapat beradaptasi dalam kehidupan kampus dengan bentuk kegiatan berupa webinar, penugasan, praktik diskusi, apresiasi serta video inspirasi dan inaugurasi sebagai acara penutup. Tahun ini PROBINMABA FMIPA mengangkat tema “EKSPLORASI” Evolusioner, Kontributif, Solutif, Peduli, Loyal, Optimis, Religius, Aktif, Sinergis, dan Inovatif",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://www.instagram.com/probinmabafmipa/",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FK-FIKES",
@@ -151,38 +152,39 @@ const dataPk = [
 		description:
 			"PKKMB FK-FIKES UB merupakan langkah awal yang akan dilalui oleh seluruh mahasiswa baru khususnya mahasiswa FK-FIKES dalam memulai kehidupan kampus supaya lebih mengenal dan mendapatkan berbagai pengalaman baru di FK FIKES UB. Kegiatan ini rutin diselenggarakan setiap tahunnya bertujuan supaya terciptanya mahasiswa yang berkarakter serta mahasiswa yang siap berkontribusi dalam masyarakat. Kegiatan PKKMB ini terdiri dari dua rangkaian utama yaitu ORDIK (Orientasi Pendidikan) dan ORMAWA (Orientasi Kemahasiswaan). Seluruh kegiatan ini akan dilaksanakan selama 2 (dua) hari melalui berbagai metode seperti webinar, talkshow, baca, tulis, diskusi, pemberian tugas, dan games.",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://www.instagram.com/probinmabafkfikesub/",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA PSDKU",
 		name: "PKKMB PSDKU",
 		fakultas: "PSDKU UB",
+		img : '/assets/fakultas/PSDKU.png',
 		description:
 			"PKKMB PSDKU merupakan kegiatan pengenalan kehidupan kampus bagi mahasiswa baru yang dilaksanakan oleh PSDKU UB Kediri. Kegiatan PKKMB ini bertujuan untuk memberikan gambaran kepada mahasiswa baru untuk dapat mengenal lebih dalam tentang kehidupan kampus dan menjadikan mahasiswa baru berintegritas sehingga dapat memberikan manfaat bagi lingkungan sekitar. Kegiatan PKKMB PSDKU UB Kediri terdiri dari 4 (empat) rangkaian utama, yaitu : <br/><br/><ol><li>PKKMB merupakan kegiatan orientasi bagi mahasiswa baru yang bertujuan supaya mahasiswa baru dapat mengenal lingkungan kampus dan kehidupan di bangku kuliah.</li><li>PBPK merupakan kegiatan yang memberi pembekalan mengenai budi pekerti dan kepemimpinan dalam masa peralihan dari SMA ke jenjang perkuliahan.</li><li>OH (Open House) dan Malam Inagurasi merupakan kegiatan yang memberikan kesempatan kepada mahasiswa baru untuk mengenal lebih dalam lagi  mengenai organisasi mahasiswa dan lembaga LKM serta dilanjut oleh kegiatan malam keakraban disini seluruh mahasiswa baru diberikan kesempatan untuk dapat mempersembahkan seluruh talenta seni yang dimiliki.</li><li>Liga Mahasiswa merupakan kegiatan pelatihan dan Pembekalan dalam penulisan karya tulis ilmiah untuk kegiatan PKM Maba yang bertujuan untuk meningkatkan kemampuan mahasiswa/i dalam menulis karya ilmiah yang baik dan benar.</li></ol>",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://www.instagram.com/probinmabafkfikesub/",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FISIP",
@@ -190,18 +192,18 @@ const dataPk = [
 		fakultas: "Fakultas Ilmu Sosial dan Ilmu Politik",
 		description: "",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://instagram.com/pkkmaba.fisipub",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FPIK",
@@ -209,18 +211,18 @@ const dataPk = [
 		fakultas: "Fakultas Perikanan dan Ilmu Kelautan",
 		description: "",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://instagram.com/pinishi2021",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FAPET",
@@ -228,57 +230,59 @@ const dataPk = [
 		fakultas: "Fakultas Peternakan",
 		description: "",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://www.instagram.com/maba_fapetub/",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FTP",
 		name: "RAJAWALI FTP",
 		fakultas: "Fakultas Teknologi Pertanian",
+		img : '/assets/fakultas/FTP.png',
 		description: "",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://instagram.com/rajawaliftpub",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FP",
 		name: "POSTER FP",
 		fakultas: "Fakultas Pertanian",
+		img : '/assets/fakultas/FP.png',
 		description:
 			"POSTER FP UB merupakan singkatan dari Program Orientasi Studi Terpadu Fakultas Pertanian Universitas Brawijaya. Kegiatan ini merupakan salah satu program dari Probin Maba FP UB 2022 sebagai wadah bagi Mahasiswa Baru dalam pengenalan dan pembinaan untuk menghadapi kehidupan kampus yang dinamis. Kegiatan POSTER FP UB 2022 berlangsung selama satu semester yang terdiri dari 3 (tiga) rangkaian utama, yaitu PK2MFP (Pengenalan Kehidupan Kampus Fakultas Pertanian), PPM (Pengembangan Potensi Mahasiswa), dan INAUGURASI (pengukuhan status mahasiswa bagi mahasiswa baru Fakultas Pertanian Universitas Brawijaya).",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://instagram.com/posterfpub",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FEB",
@@ -287,57 +291,79 @@ const dataPk = [
 		description:
 			"PKK-MABA FEB UB merupakan sarana bagi mahasiswa baru untuk dapat beradaptasi dengan lingkup kampus dan juga memberi bekal untuk mencapai keberhasilan dalam menempuh pendidikan di Perguruan Tinggi.  Kegiatan PKK MABA 2022 membangun budaya dalam berpikir kritis dan kreatif. Selain itu, menebarkan dampak positif melalui komunikasi yang efektif kepada seluruh stakeholder. Melalui kegiatan PKK MABA FEB UB diharapkan mahasiswa mengetahui dan memahami pentingnya Tri Dharma Perguruan Tinggi serta Peran dan Fungsi Mahasiswa. Tahun ini PKK-MABA FEB 2022 membawa nama Eternal.",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://www.instagram.com/eternalsfebub/",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FIA",
 		name: "PKKMABA FIA",
 		fakultas: "Fakultas Ilmu Administrasi",
+		img : '/assets/fakultas/FIA.jpeg',
 		description:
 			"PKKMB merupakan sarana bagi jajaran pemimpin perguruan tinggi untuk memperkenalkan mahasiswa baru dalam proses transisi dari siswa menjadi mahasiswa. Selain itu, PKKMB ini juga dilaksanakan untuk mempercepat proses adaptasi mahasiswa dengan lingkungan yang baru dan memberikan bekal untuk menjadi mahasiswa yang dewasa dan mandiri, serta berhasil menempuh pendidikan di perguruan tinggi.",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://instagram.com/pkkmb_fiaub",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
 	},
 	{
 		id: "PK2MABA FH",
 		name: "PKKMABA FH",
 		fakultas: "Fakultas Hukum",
+		img : '/assets/fakultas/FH.png',
 		description: "",
 		// null data
-		spotify: "https://rajabrawijaya.ub.ac.id/",
+		spotify: null,
 		// null data
-		youtube: "https://rajabrawijaya.ub.ac.id/",
+		youtube: null,
 		// null data
-		tiktok: "https://rajabrawijaya.ub.ac.id/",
+		tiktok: null,
 		// null data
-		telegram: "https://rajabrawijaya.ub.ac.id/",
+		telegram: null,
 		// null data
-		twitter: "https://rajabrawijaya.ub.ac.id/",
+		twitter: null,
 		instagram: "https://instagram.com/pk2maba.fhub",
 		// null data
-		line: "https://rajabrawijaya.ub.ac.id/",
+		line: null,
+	},
+	{
+		id: "PK2MABA FT",
+		name: "PKKMABA FT",
+		fakultas: "Fakultas Teknik",
+		img : '/assets/fakultas/FT.png',
+		description: "",
+		// null data
+		spotify: null,
+		// null data
+		youtube: null,
+		// null data
+		tiktok: null,
+		// null data
+		telegram: null,
+		// null data
+		twitter: null,
+		instagram: null,
+		// null data
+		line: null,
 	},
 ];
 
--- pages/galeri.js
+++ pages/galeri.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Nav from '../layout/nav/Nav'
 
 export default function galeri() {
+    useEffect(()=>{
+        console.log("ready")
+    })
   return (
     <>
         <Nav variant={'white'}/>
+        <div className='flex justify-center gap-3'>
+            <div className='flex flex-col'>
+                <div className='h-96 w-52 my-3 bg-black'></div>
+                <div className='h-96 w-52 my-3 bg-black'></div>
+                <div className='h-96 w-52 my-3 bg-black'></div>
+                <div className='h-96 w-52 my-3 bg-black'></div>
+                <div className='h-96 w-52 my-3 bg-black'></div>
+                <div className='h-96 w-52 my-3 bg-black'></div>
+                <div className='h-96 w-52 my-3 bg-black'></div>
+            </div>
+            <div className='flex flex-col'>
+                <div className='h-96 w-52 my-3 bg-blue'></div>
+                <div className='h-96 w-52 my-3 bg-blue'></div>
+                <div className='h-96 w-52 my-3 bg-blue'></div>
+                <div className='h-96 w-52 my-3 bg-blue'></div>
+                <div className='h-96 w-52 my-3 bg-blue'></div>
+                <div className='h-96 w-52 my-3 bg-blue'></div>
+                <div className='h-96 w-52 my-3 bg-blue'></div>
+            </div>
+        </div>
     </>
   )
 }
