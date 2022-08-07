48e481f4fb3e769c46dc1bfac77d664e69cb45ae
mfadlihs <winnerfadli999@gmail.com>
2022-08-07 17:11:54 +0700

    gallery, not repsponsive yet

--- layout/galeri/Gallery.jsx
+++ layout/galeri/Gallery.jsx
@@ -1,18 +1,130 @@
+import { useState } from "react";
+import style from "./style.module.css";
+import Image from "next/dist/client/image";
+
 import Item from "./Item";
+import GallerySlider from "../../component/galeri/GallarySlider";
+import { Pattern } from "../../constants/Images";
 
 export default function Gallery() {
+	const [isVideo, setIsVideo] = useState(true);
+
 	return (
-		<div className='flex h-screen w-screen overflow-hidden flex cursor-pointer'>
-			<div className='flex flex-col justify-between h-full py-16'>
-				<div className='flex justify-center grow items-center'>
-					<div className='flex items-center gap-5'>
-						<Item active>Video</Item>
-						<div className='w-4 h-4 bg-blue rounded-full'></div>
-						<Item>Foto</Item>
+		<div>
+			<div className='md:hidden'>
+				<div className='max-w-md sm:mb-2 mb-1 md:mb-4 mx-auto drop-shadow-md'>
+					<Image
+						src={Pattern.HeadingFull}
+						layout='responsive'
+						objectFit='cover'
+						alt='RAJA Brawijaya'
+					/>
+				</div>
+				<div className='font-agrandir font-bold md:text-6xl sm:text-3xl  text-2xl lg:text-8xl text-center text-blue'>
+					Galeri <span className='text-orange'>RAJA Brawijaya</span>
+				</div>
+				<div className='max-w-md sm:mt-2 mb-1 md:mt-4  mx-auto drop-shadow-md rotate-180'>
+					<Image
+						src={Pattern.HeadingFull}
+						layout='responsive'
+						objectFit='cover'
+						alt='RAJA Brawijaya'
+					/>
+				</div>
+			</div>
+			<div className='md:flex block h-screen w-screen overflow-hidden px-24'>
+				<div className='flex flex-col justify-between md:h-full md:py-16 '>
+					<div className='flex justify-center grow md:items-center'>
+						<div className='flex items-center gap-5'>
+							<Item active={isVideo} setIsVideo={setIsVideo} video={true}>
+								Video
+							</Item>
+							<div className='w-4 h-4 bg-blue rounded-full'></div>
+							<Item active={!isVideo} setIsVideo={setIsVideo} video={false}>
+								Foto
+							</Item>
+						</div>
+					</div>
+					<div className='hidden md:block font-agrandir text-3xl font-normal'>
+						Video rangkaian acara
+						<br />
+						RAJA Brawijaya 2022
 					</div>
 				</div>
-				<div>asd</div>
+				<div className='grow'>
+					<div className='h-full overflow-hidden'>
+						<div className='w-fit space-x-20 mx-auto'>
+							<GallerySlider />
+							<GallerySlider reverse />
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
 }
+
+/*
+<div className={`w-32 space-y-[20px] ${style.slideTrack}`}>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-blue' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-slate-600' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-yellow' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-gray-900' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-zinc-700' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-stone-500' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-red-400' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-lime-600' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-amber-500' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-emerald-500' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-blue' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-slate-600' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-yellow' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-gray-900' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-zinc-700' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-stone-500' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-red-400' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-lime-600' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-amber-500' />
+						</div>
+						<div className='slider'>
+							<div className='w-32 h-32 bg-emerald-500' />
+						</div>
+					</div>
+*/
--- layout/galeri/Item.jsx
+++ layout/galeri/Item.jsx
@@ -1,22 +1,25 @@
 import style from "./style.module.css";
 
-export default function Item({ children, active }) {
+export default function Item({ children, active, setIsVideo, video }) {
 	return (
-		<div className=''>
+		<div
+			className=''
+			onClick={() => {
+				setIsVideo(video);
+			}}
+		>
 			<div
-				className={`font-agrandir text-blue font-normal relative
-        ${style.text}
-      ${active ? "text-9xl" : "text-5xl"}
-      ${active && style.active}
+				className={`font-agrandir text-blue font-normal relative cursor-pointer ${
+					style.text
+				} ${
+					active
+						? "lg:text-9xl md:text-7xl sm:text-6xl text-5xl "
+						: "lg:text-5xl md:text-4xl sm:text-3xl text-2xl"
+				} ${active && style.active}
     `}
 			>
 				{children}
 			</div>
-			{/* {active && (
-				<div
-					className={`w-[80%] h-2 ease-in duration-700 bg-orange mx-auto rounded-lg `}
-				></div>
-			)} */}
 		</div>
 	);
 }
