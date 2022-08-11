b5612c1446af78efa34ccde536e9137a3ae83b2f
Mohammad Ibnu <siibnu99@gmail.com>
2022-08-11 12:50:53 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,8 @@ import "../style/globals.css";
 import "../style/swiperstyle.css";
 
 function MyApp({ Component, pageProps }) {
-  const musicRef = useRef(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      musicRef.current.play();
-    }, 1000);
-  }, []);
-
   return (
     <div>
-      <audio
-        ref={musicRef}
-        autoPlay
-        hidden
-        loop
-        src="/assets/sound/jingle.mp3"
-      ></audio>
       <Component {...pageProps} />
     </div>
   );
